f9ed77a28f0fbaaf7acc5216369697eef61a2df7
Martin Kapun <capoony@gmail.com>
2023-07-03 12:19:25 +0200

    update

--- Analyses/scripts/PlotTp_adult_flat_Fig3.R
+++ Analyses/scripts/PlotTp_adult_flat_Fig3.R
@@ -8,6 +8,9 @@ library(multcomp)
 library(car)
 library(readxl)
 
+setwd("/Users/martinkapun/Documents/GitHub/DrosophilaThermalGradient/Analyses")
+
+
 DATA <- read_excel("data/Strunov_etal_WolbTP_2023_RawData.xlsx", sheet = "adult_flat")
 DATA$replica <- as.factor(DATA$replica)
 DATA$exp_date <- as.factor(DATA$exp_date)
@@ -22,16 +25,25 @@ means <- DATA %>%
   dplyr::summarise(Mean = mean(TempEst), SD = sd(TempEst), Median = median(TempEst))
 means
 
-labels <- c("w+" = "wMel", "w2+" = "wMelCS", "w3+" = "wMelPop")
 
-ggplot(DATA, aes(x = infection, y = TempEst, col = infection)) +
-  geom_jitter() +
-  geom_boxplot(alpha = 0.2) +
+labels <- c("w-" = "w-", "w+" = "wMel", "w2+" = "wMelCS", "w3+" = "wMelPop")
+DATA$infection <- as.factor(DATA$infection)
+levels(DATA$infection) <- labels
+
+PLOT <- ggplot(DATA, aes(x = infection, y = TempEst, col = infection)) +
+  geom_boxplot() +
+  geom_point(aes(color = infection), position = position_jitterdodge(0.8), alpha = 0.5) +
   theme_classic() +
   theme(text = element_text(size = 15)) +
-  xlab("Infection type") +
+  xlab("Infection Type") +
+  labs(color = "Infection Type") +
   scale_y_continuous(name = "Temperature (°C)", breaks = seq(10, 36, 1)) +
-  scale_colour_manual(values = c("#999999", "blue3", "firebrick3", "orange"), labels = labels)
+  scale_colour_manual(values = c("darkgrey", "blue3", "firebrick3", "orange"), labels = labels)
+
+ggsave("results/Figure3.pdf", PLOT, width = 8, height = 4)
+ggsave("results/Figure3.png", PLOT, width = 8, height = 4)
+
+
 
 cat("\n**** Linear mixed model ****\n")
 
